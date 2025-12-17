0d2d3f204c12b491baf0296500a00ed27121db0e
Sofia-000 <sofi.simons2508@gmail.com>
2025-12-17 22:24:29 +0300

    господи помоги Домашка №10

---
+++ 10_lesson/test_database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, inspect
+
+
+db_connection_string = "postgresql://qa:skyqa@5.101.50.27:5432/x_clients"
+db = create_engine(db_connection_string)
+
+def test_db_connection():
+# Используем инспектор для получения информации о таблицах
+	inspector = inspect(db)
+	names = inspector.get_table_names()
+	assert names[2] == 'app_users'
